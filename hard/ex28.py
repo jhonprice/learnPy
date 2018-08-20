d85e2eaef8ace6966ac44781583aed24a2aaff74
jhonprice <a718329809@live.com>
2018-08-20 22:54:52 +0800

    23-29

---
+++ hard/ex28.py
@@ -0,0 +1,13 @@
+True and True
+False and True
+1==1 and 2==1
+"test" == "test"
+1==1 or 2!=1
+True and 1==1
+False and 0!=0
+True and 1==1
+"test" == "testing"
+1!=0 and 2==1
+"test" =="testing"
+
+not (True and False)
